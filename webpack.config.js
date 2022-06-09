52727bc92ab4e1de4624822a569951ed89cc4cd8
meetdevlope <meetjpatel2412@gmail.com>
2022-06-09 16:24:58 +0530

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'bundle'),
-    filename: 'master.js',
-  },
-  resolve: {
-    extensions: ['.js', '.json', '.jsx'],
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'bundle'),
+        filename: 'master.js',
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.jsx'],
+    },
+    mode: 'production',
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  devServer: {
-    port: 9000,
-    open: true,
-  },
+    devServer: {
+        port: 9000,
+        open: true,
+    },
 };
