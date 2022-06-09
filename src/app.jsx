451f3163d443c0b05cba39cd8d816288910560bc
meetdevlope <meetjpatel2412@gmail.com>
2022-06-09 16:41:41 +0530

    update-2

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import Products from './components/Products';
 import Slider from './components/Slider';
+import './style.css'
 
 function app() {
   return (
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -30,22 +30,22 @@ export default class AutoPlayMethods extends Component {
       <div className="flex justify-center">
         <Slider className="slider-div w-full flex-1" {...settings}>
           <div>
-            <img src="/Assets/B1.jpeg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wearables/Unrec/Hero/Smartwatches/Under/1999/Smartwatches_Homepage_DesktopHeroTemplate_3000x1200._CB634570851_.jpg" alt="Banner" />
           </div>
           <div>
-            <img src="/Assets/B2.jpg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/WLA/June/MonsoonSale/GWHero/PocketFriendly/D47281209_WLA_Monsoon_Sale_DesktopTallHero_3000x1200_2._CB636244778_.jpg" alt="Banner" />
           </div>
           <div>
-            <img src="/Assets/B3.jpg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Beauty/GW/June/MonsoonCarnival/Homepage_DesktopHero-3000x1200-skincare._CB636240735_.jpg" alt="Banner" />
           </div>
           <div>
-            <img src="/Assets/B4.jpg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonSaheli/Gift/sec2/2x._CB646912821_.jpg" alt="Banner" />
           </div>
           <div>
-            <img src="/Assets/B5.jpg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/media/MonsoonMiniART_June/Homepage_DesktopHeroTemplate_3000x1200_Unrec_2x._CB636267028_.jpg" alt="Banner" />
           </div>
           <div>
-            <img src="/Assets/B6.jpg" alt="Banner" />
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/SingleTitle/KGFChapter2file/Alllanguages/3000x1200_Hero-Tall_NP._CB635770012_.jpg" alt="Banner" />
           </div>
         </Slider>
       </div>
